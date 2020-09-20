99649a79fc632a277ab0f8a28c69870a36925211
shirleylidman.sl@gmail.com <shirleylidman.sl@gmail.com>
2020-09-20 15:39:33 +0200

    listview

--- FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Views/HomePage2.xaml.cs
+++ FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Views/HomePage2.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FullSteamAheadMVP0Project.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,9 +13,25 @@ namespace FullSteamAheadMVP0Project.Views
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Homepage2 : ContentPage
     {
+        public IList<Team> Teams { get; private set; }
         public Homepage2()
         {
             InitializeComponent();
+
+            Teams = new List<Team>();
+
+            BindingContext = this;
+
+            
+        }
+        void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+        {
+            Team selectedItem = e.SelectedItem as Team;
+        }
+
+        void OnListViewItemTapped(object sender, ItemTappedEventArgs e)
+        {
+            Team tappedItem = e.Item as Team;
         }
 
     }
