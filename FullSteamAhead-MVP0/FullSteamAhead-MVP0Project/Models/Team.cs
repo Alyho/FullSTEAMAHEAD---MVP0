17151f48c9669d6f690444837cf9868e2e64d695
Shannon Wang <shannonwang1029@gmail.com>
2020-09-27 11:09:23 -0700

    change strings

--- FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Models/Team.cs
+++ FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Models/Team.cs
@@ -8,6 +8,10 @@ namespace FullSteamAheadMVP0Project.Models
         public Team()
         {
             Team_Members = new List<User>();
+            Team_Username = "";
+            Team_Nickname = "";
+            Team_Password = "";
+            Team_Information = new TeamInformation();
         }
         public string Team_Username { get; set; }
         public string Team_Nickname { get; set; }
--- FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Models/TeamInformation.cs
+++ FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Models/TeamInformation.cs
@@ -3,11 +3,22 @@ namespace FullSteamAheadMVP0Project.Models
 {
     public class TeamInformation
     {
+        public TeamInformation()
+        {
+            Team_Email = "";
+            State = "";
+            City = "";
+            Zip_Code = "";
+            Phone_Number = "";
+            Gender = "";
+            Privacy = "";
+            Bio = "";
+        }
         public string Team_Email { get; set; }
         public string State { get; set; }
         public string City { get; set; }
-        public int Zip_Code { get; set; }
-        public int Phone_Number { get; set; }
+        public string Zip_Code { get; set; }
+        public string Phone_Number { get; set; }
         public string Gender { get; set; }
         public string Privacy { get; set; }
         public string Bio { get; set; }
