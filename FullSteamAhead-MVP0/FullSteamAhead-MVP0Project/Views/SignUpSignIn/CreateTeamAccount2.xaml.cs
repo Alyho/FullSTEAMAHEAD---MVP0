bcfe5e315751bbab6c977b2196f582a0cd76d3b4
shirleylidman.sl@gmail.com <shirleylidman.sl@gmail.com>
2021-03-31 21:03:21 +0200

    Added student preferences and started on team sign up

---
+++ FullSteamAhead-MVP0/FullSteamAhead-MVP0Project/Views/SignUpSignIn/CreateTeamAccount2.xaml.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FullSteamAheadMVP0Project.Views
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+public partial class CreateTeamAccount2 : ContentPage
+{
+    public CreateTeamAccount2()
+    {
+        InitializeComponent();
+    }
+}
+}
