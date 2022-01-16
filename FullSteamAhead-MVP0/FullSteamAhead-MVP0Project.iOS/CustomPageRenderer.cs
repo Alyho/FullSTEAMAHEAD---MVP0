275bdf4954aaccfc1e9460946213482ba93e3c05
shirleylidman.sl@gmail.com <shirleylidman.sl@gmail.com>
2022-01-16 20:14:17 +0100

    Disabled dark mode (iOS) + online checkbox

---
+++ FullSteamAhead-MVP0/FullSteamAhead-MVP0Project.iOS/CustomPageRenderer.cs
@@ -0,0 +1,23 @@
+﻿using Foundation;
+using FullSteamAheadMVP0.iOS;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly:ExportRenderer(typeof(ContentPage), typeof(CustomPageRenderer))]
+namespace FullSteamAheadMVP0.iOS
+{
+    public class CustomPageRenderer : PageRenderer
+    {
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+            if (UIDevice.CurrentDevice.CheckSystemVersion(13,0))
+                OverrideUserInterfaceStyle = UIUserInterfaceStyle.Light;
+        }
+    }
+}
